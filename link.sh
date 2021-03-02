18fc144e6edae27fc9c69bbb31c3eab31c97abf3
Shun_Harutaro <n.shunshun2004@gmail.com>
2021-03-02 20:51:55 +0900

    add link.sh

---
+++ link.sh
@@ -0,0 +1,5 @@
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
