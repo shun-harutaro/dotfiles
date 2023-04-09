9483b4eb72068780fa3cc91615883aa89f146a79
shun-harutaro <shun_harutaro@protonmail.com>
2023-04-09 12:59:35 +0900

    added config for theme in vim

---
+++ iceberg.sh
@@ -0,0 +1,4 @@
+mkdir $HOME/.vim
+mkdir $HOME/.vim/colors
+git clone https://github.com/cocopon/iceberg.vim.git $HOME/.vim/iceberg.vim
+cp -f $HOME/.vim/iceberg.vim/colors/iceberg.vim $HOME/.vim/colors/iceberg.vim
