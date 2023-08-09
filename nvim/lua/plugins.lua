4563bef53047faa95122d8d0ca061a2538ac78da
shun-harutaro <shun_harutaro@protonmail.com>
2023-08-09 13:17:31 +0900

    add filebrowser

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -18,16 +18,20 @@ require('jetpack.packer').add {
     { 'lewis6991/gitsigns.nvim' },
     { 'lukas-reineke/indent-blankline.nvim' },
     { 'j-hui/fidget.nvim', tag = 'legacy' },
+    { 'https://git.sr.ht/~whynothugo/lsp_lines.nvim' },
 
     -- utility
+    -- cmp
     { 'hrsh7th/nvim-cmp' },
     { 'hrsh7th/cmp-nvim-lsp' },
     { 'hrsh7th/cmp-buffer' },
     { 'hrsh7th/cmp-path' },
     { 'hrsh7th/cmp-cmdline' },
+    -- others
     { 'nvim-telescope/telescope.nvim', tag = '0.1.2', requires = 'nvim-lua/plenary.nvim' },
     { 'nvim-treesitter/nvim-treesitter' },
     { 'windwp/nvim-autopairs' },
+    { 'nvim-telescope/telescope-file-browser.nvim' },
 
     -- LSP
     { 'neovim/nvim-lspconfig' },
@@ -39,6 +43,17 @@ require('lualine').setup()
 require('gitsigns').setup()
 require('fidget').setup()
 require('nvim-autopairs').setup()
+require('lsp_lines').setup()
+require('telescope').load_extension "file_browser"
+
+require'nvim-treesitter.configs'.setup({
+    highlight = {
+        enable = true,
+    },
+    Indent = {
+        eneble = true,
+    }
+})
 
 local lspconfig = require('lspconfig')
 --lspconfig.pyright.setup {}
@@ -78,3 +93,11 @@ cmp.setup({
         { name = "buffer" },
     })
 })
+
+vim.diagnostic.config({ virtual_text = false })
+vim.api.nvim_set_keymap(
+    "n",
+    "fb",
+    ":Telescope file_browser<CR>",
+    { noremap = true }
+)
